dd8f331c04077ad6205c19009d8e674d141b69c6
Park Chan <907339772@qq.com>
2017-01-25 22:24:10 +0800

    add SQL model 1st

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/user/bin/env python
 #-*- coding: utf-8 -*-
-
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -8,13 +8,32 @@ from flask_moment import Moment
 from flask_wtf import Form 
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask (__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+		
+		
 
 class NameForm(Form):
 	

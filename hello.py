f2b019dd7d49f86288d3e3ea7d856b988519256d
Park Chan <907339772@qq.com>
2017-01-25 21:17:56 +0800

    add flask wtforms

--- hello.py
+++ hello.py
@@ -1,24 +1,36 @@
 #!/user/bin/env python
 #-*- coding: utf-8 -*-
-from datetime import datetime
+
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form 
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask (__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-	return render_template('index.html',
-						   current_time=datetime.utcnow())
+class NameForm(Form):
+	
+	name = StringField('What is your name?', validators = [Required()])
+	submit = SubmitField('Submit')
+		
 
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name = name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
